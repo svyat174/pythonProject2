e448e4ff4dae725c05b0f58aeb4554ab2248daba
svyat777 <vas-ivano2@inbox.ru>
2022-07-29 13:40:48 +0500

    fix range

--- proba.py
+++ proba.py
@@ -1,11 +1,11 @@
 import random
 number = random.randint(1, 10)
-for kk in range(5):
+for kk in range(3):
     kk = int(input('enter num: '))
     if kk < number:
-        print('less')
+        print('you num is less number')
     if kk > number:
-        print('more')
+        print('you num is more number')
     if kk == number:
         print('you are win!!!')
         break
