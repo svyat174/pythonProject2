65df403e47814831ea443e1e0de3f24853c21e25
svyat777 <vas-ivano2@inbox.ru>
2022-07-29 13:38:51 +0500

    123

--- proba.py
+++ proba.py
@@ -1,1 +1,13 @@
-print('hi')
+import random
+number = random.randint(1, 10)
+for kk in range(5):
+    kk = int(input('enter num: '))
+    if kk < number:
+        print('less')
+    if kk > number:
+        print('more')
+    if kk == number:
+        print('you are win!!!')
+        break
+else:
+    print('you lose!!!')
