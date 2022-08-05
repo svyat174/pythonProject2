7478b2a8991817ff1c51f93acb1224e510385d6b
svyat777 <vas-ivano2@inbox.ru>
2022-08-05 14:44:59 +0500

    1 stage

---
+++ part 6/Replase_input_text.py
@@ -0,0 +1,13 @@
+text = 'яамибюЛ акнитсирК'
+a = []
+len_a = []
+b = ''
+n = 0
+for i in text.split():
+    a.append('* ' + i[::-1])
+    len_a.append(len(a[n]))
+    n += 1
+for kk in a:
+    b += kk + (max(len_a) -len(kk) +1) * ' ' + '*' + '\n'
+b = '*' * (max(len_a) + 2) + '\n' + b + '*' * (max(len_a) + 2)
+print(b)
