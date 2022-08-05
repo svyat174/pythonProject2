ab63d467107018f50d343e5190b9f692e59e44a9
svyat777 <vas-ivano2@inbox.ru>
2022-08-05 16:17:09 +0500

    fix PEP8 errors

--- part 6/Replase_input_text.py
+++ part 6/Replase_input_text.py
@@ -8,6 +8,6 @@ for i in text.split():
     len_a.append(len(a[n]))
     n += 1
 for kk in a:
-    b += kk + (max(len_a) -len(kk) +1) * ' ' + '*' + '\n'
+    b += kk + (max(len_a) - len(kk) + 1) * ' ' + '*' + '\n'
 b = '*' * (max(len_a) + 2) + '\n' + b + '*' * (max(len_a) + 2)
 print(b)
